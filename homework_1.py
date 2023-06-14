685a82035c526595da02c33736c7cd9bd756aecf
«Pashkov <pashkov921326@gmail.com>
2023-06-14 21:28:29 +0300

    first commit

--- homework_1.py
+++ homework_1.py
@@ -1,31 +1,31 @@
 # Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
 
-def heapify(arr, n, i):
-    largest = i
+def heapif(arr, n, i):
+    large = i
     left = 2 * i + 1
     right = 2 * i + 2
 
-    if left < n and arr[left] > arr[largest]:
-        largest = left
+    if left < n and arr[left] > arr[large]:
+        large = left
 
-    if right < n and arr[right] > arr[largest]:
-        largest = right
+    if right < n and arr[right] > arr[large]:
+        large = right
 
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
+    if large != i:
+        arr[i], arr[large] = arr[large], arr[i]
+        heapif(arr, n, large)
 
 def heap_sort(arr):
     n = len(arr)
 
     for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
+        heapif(arr, n, i)
 
     for i in range(n - 1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, 0)
+        heapif(arr, i, 0)
 
-arr = [12, 11, 13, 5, 6, 7]
+arr = [11, 14, 13, 5, 6, 7]
 heap_sort(arr)
 print("Отсортированный массив:")
 print(arr)
